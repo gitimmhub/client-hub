5f5febfa32feda4ce999dfe95cafc4910cc327e3
Matheus Armando Timm Barbieri <matheus-tb@edu.univali.br>
2026-08-20 16:35:55 -0300

    version 1.3.0

--- client-hub.php
+++ client-hub.php
@@ -3,7 +3,7 @@
  * Plugin Name: Client Hub
  * Plugin URI: https://github.com/gitimmhub/client-hub
  * Description: Portal do cliente integrado ao CSP para acesso a orçamentos e estudos.
- * Version: 1.2.0
+ * Version: 1.3.0
  * Author: Matheus Barbiéri
  * Author URI: https://github.com/gitimmhub
  * Text Domain: client-hub
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('CLIENT_HUB_VERSION', '1.1.2');
+define('CLIENT_HUB_VERSION', '1.3.0');
 define('CLIENT_HUB_FILE', __FILE__);
 define('CLIENT_HUB_PATH', plugin_dir_path(__FILE__));
 define('CLIENT_HUB_URL', plugin_dir_url(__FILE__));
@@ -198,7 +198,20 @@ function client_hub_login(): void
      * Em produção, troque pela URL pública do CSP.
      */
     $api_url = 'https://wgb.csp.app.br/api/client-hub/login';
+    //$api_url = 'https://wgbdev.maestro.local/api/client-hub/login';
 
+    /*
+    $host = $_SERVER['HTTP_HOST'] ?? '';
+    $host = explode(':', $host)[0];
+    if ($host === 'wordpress.local') {
+        $api_url = 'https://wgbdev.maestro.local/api/client-hub/login';
+    } else {
+        $partes = explode('.', $host);
+        $subdominio = (count($partes) > 2) ? $partes[0] : 'default';
+
+        $api_url = "https://{$subdominio}.csp.app.br/api/client-hub/login";
+    }
+    */
     $response = wp_remote_post($api_url, [
         'timeout' => 20,
 
