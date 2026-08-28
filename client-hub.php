5d31bc3b86a39b3b878ed0d0dd20eb82772fb73e
Matheus Armando Timm Barbieri <matheus-tb@edu.univali.br>
2026-08-28 08:39:30 -0300

    remove nonce

--- client-hub.php
+++ client-hub.php
@@ -3,7 +3,7 @@
  * Plugin Name: Client Hub
  * Plugin URI: https://github.com/gitimmhub/client-hub
  * Description: Portal do cliente integrado ao CSP para acesso a orçamentos e estudos.
- * Version: 1.4.5
+ * Version: 1.4.6
  * Author: Matheus Barbiéri
  * Author URI: https://github.com/gitimmhub
  * Text Domain: client-hub
@@ -25,7 +25,7 @@ $updateChecker = PucFactory::buildUpdateChecker(
 
 $updateChecker->setBranch('main');
 
-define('CLIENT_HUB_VERSION', '1.4.5');
+define('CLIENT_HUB_VERSION', '1.4.6');
 define('CLIENT_HUB_FILE', __FILE__);
 define('CLIENT_HUB_PATH', plugin_dir_path(__FILE__));
 define('CLIENT_HUB_URL', plugin_dir_url(__FILE__));
@@ -197,12 +197,12 @@ function client_hub_login(): void
     /*
      * Valida o nonce sem encerrar com o "-1" padrão do WordPress.
      */
-    if (!check_ajax_referer('client_hub_login', 'nonce', false)) {
-        wp_send_json([
-            'success' => false,
-            'message' => 'Sessão ou token de segurança expirado. Atualize a página e tente novamente.',
-        ], 403);
-    }
+    //if (!check_ajax_referer('client_hub_login', 'nonce', false)) {
+    //    wp_send_json([
+    //        'success' => false,
+    //        'message' => 'Sessão ou token de segurança expirado. Atualize a página e tente novamente.',
+    //    ], 403);
+    //}
 
     /*
      * Garante que a sessão PHP esteja ativa.
